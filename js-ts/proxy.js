388ebca5b6389cd5739db1d9e571100978f09532
HanShuaiHu <335672336@qq.com>
2022-06-10 18:03:32 +0800

    ts-interface

--- js-ts/proxy.js
+++ js-ts/proxy.js
@@ -1,14 +1,16 @@
 const obj1 = {
-    count:1
+  count: 1,
+  print: () => console.log('obj1中的方法')
 }
 const obj2 = {
-    count:2
+  count: 2
 }
 
-let proxy = new Proxy(obj1,{
-    get(target,key){
-        console.log(target,key);
-    }
+let proxy = new Proxy(obj1, {
+  get (target, key) {
+    console.log(target, key, '拦截')
+    console.log(this, 'this')
+  }
 })
 
 proxy.count
